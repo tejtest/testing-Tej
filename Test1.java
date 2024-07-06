137bf9f241daf5a26f8c913a76c5d4bea3ee67f4
tejtest <TejaswiniTest24@gmail.com>
2024-07-06 09:42:57 +0530

    Create Test1.java

---
+++ Test1.java
@@ -0,0 +1,35 @@
+package com.qa.project1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void method1() throws Exception {
+		Reporter.log("Testcase ID= TC01");
+		RemoteWebDriver driver= new ChromeDriver();
+		Reporter.log("The browser is opened now.");
+		driver.get("http://dev-ec-web-admin.s3-website.ap-south-1.amazonaws.com");
+		Thread.sleep(3000);
+		Reporter.log("The application website is opened.");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		Reporter.log("The browser is maximized.");
+		driver.findElement(By.xpath("//a[@class='readTitle'][text()='Contact']")).click();
+		Thread.sleep(3000);
+		Reporter.log("Contact Tab at the top of the page has been clicked.");
+		String ExpectedTitle ="Contact Us | Educase Global School";
+		String ActualTitle = driver.getTitle();
+		Assert.assertEquals(ActualTitle, ExpectedTitle);
+		Reporter.log("Expected Result obtained");
+		driver.quit();
+		Reporter.log("The browser has been closed.");
+	}
+
+
+}
